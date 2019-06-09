0de841bec4454eac71f5afb0edf23a66a540ec2c
G VIVEK <vivek19a99@gmail.com>
2019-06-09 10:13:24 +0530

    Add files via upload

---
+++ median.py
@@ -0,0 +1,6 @@
+#using numerical python since no specific method is mentioned
+from numpy import median
+n1=int(input("Enter num 1 :"));
+n2=int(input("Enter num 2 :"));
+n3=int(input("Enter num 3 :"));
+print("The median of the numbers is: ", median([n1,n2,n3]));
