e5397a568a59fcb30967b8c975aaf70f91c4891d
G VIVEK <vivek19a99@gmail.com>
2019-06-09 08:55:22 +0530

    Add files via upload

---
+++ uplow.py
@@ -0,0 +1,10 @@
+def lowercase():
+    print("The input given in lowercase is :");
+    print(str_input.lower());
+def uppercase():
+    print("The input in uppercasev is :");
+    print(str_input.upper());
+
+str_input=str(input("Enter the string to convert to upper and lower cases :"));
+lowercase();
+uppercase();
